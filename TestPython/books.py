b6fa229e3ccdd79930e1333e2ca19d6c826cc31b
Raiyan-sharif <raiyan.sharif.1234@gmail.com>
2025-02-13 10:01:26 +0600

    update

--- TestPython/books.py
+++ TestPython/books.py
@@ -16,5 +16,28 @@ async def first_api():
     return {'message' : 'Hello Raiyan!'}
 
 @app.get("/books")
-def first_api():
+def read_all_books():
     return BOOKS
+
+@app.get("/books/{title}")
+def read_book(title: str):
+    for book in BOOKS:
+        if book.get('title').casefold() == title.casefold():
+            return  book
+
+@app.get("/books/")
+async def read_category_by_query(category: str):
+    books_to_return = []
+    for book in BOOKS:
+        if book.get('category').casefold() == category.casefold():
+            books_to_return.append(book)
+    return  books_to_return
+
+@app.get("/books/{book_author}")
+async def read_author_category_by_query(book_author: str, category: str):
+    book_to_return = []
+    for book in BOOKS:
+        if book.get('author').casefold() == book_author.casefold() and \
+            book.get('category').casefold() == category.casefold():
+            book_to_return.append(book)
+    return  book_to_return
